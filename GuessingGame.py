78bc1a44c43451fa2a1316ed37114acce9b1b9e6
pacarbino <filosophy@gmail.com>
2022-04-06 15:23:22 -0500

    backup

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,5 +1,6 @@
 # word variables
 word = "alphabet" # "generate" word
+wordArray = ['a','l','p','h','a','b','e','t'] # turn word into array (make code later)
 wordLen = len(word) #count word length
 wordDisp = ("_" * wordLen) # create word display
 wordAlert = "Your word has" + str(wordLen) + "letters."
@@ -10,7 +11,20 @@ numGuesses = 7 # statring number of guesses
 numGuessesAlert = "You have " + str(numGuesses) + " guesses remaining." # Number of guesses left message
 
 # displays
-print(numGuessesAlert) # test print
-print(wordDispAlert) # test print
 
-guess = input("What letter would you like to guess?: ")
+guessed = []
+print(guessed)
+
+while numGuesses > 0:
+    print(numGuessesAlert) # tell user number of guesses left
+    print(wordDispAlert) # show user progress
+    print("These are the letters you've already guessed:" + str(guessed))
+    guess = input("What letter would you like to guess?: ") # get user input
+        if guess in guessed = True:
+            
+    print(str("You have guessed " + guess + "!"))#test
+    guessed.append(guess)
+    print(str(guessed))#test 
+    numGuesses += 1
+
+guessed = []
