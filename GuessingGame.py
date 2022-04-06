84b30895ba2998c35fc6338a46dc7bbe808fc7f7
pacarbino <filosophy@gmail.com>
2022-04-06 12:05:37 -0500

    update GuessingGame

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,15 +1,16 @@
 # word variables
 word = "alphabet" # "generate" word
-wordLen = len(word)
+wordLen = len(word) #count word length
 wordDisp = ("_" * wordLen) # create word display
 wordAlert = "Your word has" + str(wordLen) + "letters."
 wordDispAlert = "This is what you've got so far: " + str(wordDisp)
 
 # number variables
 numGuesses = 7 # statring number of guesses
-numGuessesAlert = "You have " + str(numGuesses) + " left." # Number of guesses left message
+numGuessesAlert = "You have " + str(numGuesses) + " guesses remaining." # Number of guesses left message
 
+# displays
 print(numGuessesAlert) # test print
-
 print(wordDispAlert) # test print
 
+guess = input("What letter would you like to guess?: ")
