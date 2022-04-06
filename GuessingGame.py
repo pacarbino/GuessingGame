e2371e04a8fcb2489c623ec728edd752647f44fd
pacarbino <filosophy@gmail.com>
2022-04-06 11:45:07 -0500

    progress save on GuessingGame

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,7 +1,15 @@
-word = "alphabet"
+# word variables
+word = "alphabet" # "generate" word
+wordLen = len(word)
+wordDisp = ("_" * wordLen) # create word display
+wordAlert = "Your word has" + str(wordLen) + "letters."
+wordDispAlert = "This is what you've got so far: " + str(wordDisp)
 
-numGuesses = 7
+# number variables
+numGuesses = 7 # statring number of guesses
+numGuessesAlert = "You have " + str(numGuesses) + " left." # Number of guesses left message
 
-print("Okay, you have " + numGuesses + " left.")
+print(numGuessesAlert) # test print
 
-print(len(word))
+print(wordDispAlert) # test print
+
